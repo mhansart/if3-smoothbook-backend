2ee99b41b6807a319b98a48d030e64bb88f20a51
MarineH <marine.hansart@tastyoo.com>
2022-01-25 20:28:46 +0100

    procfile

--- server.js
+++ server.js
@@ -99,7 +99,8 @@ require('dotenv').config();
 //   }
 //   res.send(file);
 // });
-console.log(process.env.DB_HOST);
+mysql://bdedbe7e2bf65a:186e09d3@eu-cdbr-west-02.cleardb.net/heroku_f90ffa3af12f7e2?reconnect=true
+console.log(process.env);
 mysql
   .createConnection({
     host: process.env.DB_HOST,
