2705917863814ccdb471f954b01637035d319f26
MarineH <marine.hansart@tastyoo.com>
2022-01-27 00:15:17 +0100

    procfile

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
   next();
