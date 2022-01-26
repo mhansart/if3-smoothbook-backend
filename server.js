316a593cc23fd29f55a0e1416bc7bd7b6bdda31c
MarineH <marine.hansart@tastyoo.com>
2022-01-26 13:00:13 +0100

    procfile

--- server.js
+++ server.js
@@ -39,8 +39,10 @@ const bcrypt = require("bcryptjs");
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
