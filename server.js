0311966353eed29511b8f33461de2ac6cb16f1ef
MarineH <marine.hansart@tastyoo.com>
2022-02-21 21:58:44 +0100

    try

--- server.js
+++ server.js
@@ -45,5 +45,6 @@ require("./app/controllers/login.controller.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => {
+  console.log(process.env);
   console.log(`Server is running on port ${PORT}.`);
 });
